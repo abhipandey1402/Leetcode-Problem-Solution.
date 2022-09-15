c47491c244c48ebe722d565c750367f327526b3a
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2022-09-15 16:31:13 +0530

    Time: 3 ms (43.99%), Space: 54.6 MB (60.23%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       
+        for(int i=0, j=0; j<nums.length; j++){
+            if(nums[j] != 0){
+                nums[i] = nums[j];
+                if(i++ != j){
+                    nums[j] = 0;
+                }
+            }
+        }
+    }
+}
