76d51a6e3867b7aa7f87493cf311beda766cd88a
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2022-10-28 22:01:07 +0530

    Time: 14 ms (50.42%), Space: 48.2 MB (62.50%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+      int n = asteroids.length;
+        Stack<Integer> st = new Stack<>();
+        for(int i : asteroids){
+            if(i > 0){
+                st.push(i);
+            }
+           else{
+           while(!st.isEmpty() && st.peek() > 0 && Math.abs(st.peek()) < Math.abs(i)){
+               st.pop();
+           }
+               if(st.isEmpty() || st.peek() < 0){
+                   st.push(i);
+               }
+               else if(st.peek() == Math.abs(i)){
+                   st.pop();
+               }
+            } 
+        }
+        int ans[] = new int[st.size()];
+        for(int i = st.size() - 1; i >= 0; i--){
+            ans[i] = st.pop();
+        }
+        return ans;
+    }
+}
